899da6b79e6c5676560a43443ac3269a792fc363
VladislavPechen <pechenvladislav@gmail.com>
2016-11-15 18:37:40 +0300

    Working on Curves
    in progress

---
+++ Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BezierCurve : MonoBehaviour {
+
+    public Vector3[] points;
+
+	private void Reset()
+    {
+        points = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),
+            new Vector3(0f, 1f, 0f),
+            new Vector3(2f, -1f, 0f),
+            new Vector3(2f, 0f, 0f)
+        };
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+    }
+}
---
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(BezierCurve))]
+public class BezierCurveInspector : Editor {
+
+    private BezierCurve curve;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+	private void OnSceneGUI()
+    {
+        curve = target as BezierCurve;
+        handleTransform = curve.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Global ? Quaternion.identity : handleTransform.rotation;
+
+        Vector3 p0 = ShowPoint(0);
+        Vector3 p1 = ShowPoint(1);
+        Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
+
+        Handles.color = Color.gray;
+        Handles.DrawLine(p0, p1);
+        Handles.DrawLine(p2, p3);
+
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 4f);
+    }
+
+    private Vector3 ShowPoint(int index)
+    {
+        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
+        EditorGUI.BeginChangeCheck();
+        point = Handles.PositionHandle(point, handleRotation);
+        if(EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(curve, "Move Point");
+            curve.points[index] = handleTransform.InverseTransformPoint(point);
+        }
+        return point;
+    }
+}
