9fcfa11ed652894fcca66291e6024c09e6a35d8c
VladislavPechen <pechenvladislav@gmail.com>
2016-11-15 21:12:42 +0300

    BezierBase

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -1,11 +1,65 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 public class BezierCurve : MonoBehaviour {
 
-    public Vector3[] points;
+    [SerializeField]
+    private Vector3[] points;
 
-	private void Reset()
+    public int PointsCount
+    {
+        get
+        {
+            return points.Length;
+        }
+    }
+
+    public Vector3 GetPoint(int index)
+    {
+        return points[index];
+    }
+
+    public void SetPoint(int index, Vector3 point)
+    {
+        if (index % 3 == 0)                        //если выбрана средняя, то перемщать соседние точки
+        {
+            Vector3 delta = point - points[index];
+            if (index > 0)
+            {
+                points[index - 1] += delta;
+            }
+            if (index + 1 < points.Length)
+            {
+                points[index + 1] += delta;
+            }
+        }
+        points[index] = point;
+        EnforceMode(index);
+    }
+
+    public int CurveCount
+    {
+        get
+        {
+            return (points.Length - 1) / 3;         //только если добавляем три точки
+        }
+    }
+
+    [SerializeField]
+    private BezierControlPointMode[] modes;
+
+    public BezierControlPointMode GetPointMode(int index)
+    {
+        return modes[(index + 1) / 3];              //только если добавляем три точки
+    }
+
+    public void SetPointMode(int index, BezierControlPointMode mode)
+    {
+        modes[(index + 1) / 3] = mode;              //только если добавляем три точки
+        EnforceMode(index);
+    }
+
+    private void Reset()
     {
         points = new Vector3[]
         {
@@ -14,10 +68,62 @@ public class BezierCurve : MonoBehaviour {
             new Vector3(2f, -1f, 0f),
             new Vector3(2f, 0f, 0f)
         };
+
+        modes = new BezierControlPointMode[] {
+            BezierControlPointMode.Free,
+            BezierControlPointMode.Free
+        };
     }
 
     public Vector3 GetPoint(float t)
     {
         return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+    }
+
+    public void AddCurve()
+    {
+        Vector3 point = points[points.Length - 1];
+        Array.Resize(ref points, points.Length + 3);
+        point.y += 1f;
+        points[points.Length - 3] = point;
+        point.x += 2f;
+        point.y -= 2f;
+        points[points.Length - 2] = point;
+        point.y += 1f;
+        points[points.Length - 1] = point;
+
+        Array.Resize(ref modes, modes.Length + 1);
+        modes[modes.Length - 1] = modes[modes.Length - 2];
+        EnforceMode(points.Length - 4);
+    }
+
+    private void EnforceMode(int index)
+    {
+        int modeIndex = (index + 1) / 3;
+        BezierControlPointMode mode = modes[modeIndex];
+        if (mode == BezierControlPointMode.Free || modeIndex == 0 || modeIndex == modes.Length - 1)
+        {
+            return;
+        }
+
+        int middlePointIndex = modeIndex * 3;
+        int fixedIndex, enforcedIndex;
+        if (index <= middlePointIndex)
+        {
+            fixedIndex = middlePointIndex - 1;
+            enforcedIndex = middlePointIndex + 1;
+        }
+        else {
+            fixedIndex = middlePointIndex + 1;
+            enforcedIndex = middlePointIndex - 1;
+        }
+
+        Vector3 middle = points[middlePointIndex];
+        Vector3 enforcedTangent = middle - points[fixedIndex];
+        if (mode == BezierControlPointMode.Aligned)
+        {
+            enforcedTangent = enforcedTangent.normalized * Vector3.Distance(middle, points[enforcedIndex]);
+        }
+        points[enforcedIndex] = middle + enforcedTangent;
     }
 }
--- Assets/Scripts/Editor/BezierCurveInspector.cs
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -3,39 +3,98 @@ using UnityEditor;
 using System.Collections;
 
 [CustomEditor(typeof(BezierCurve))]
-public class BezierCurveInspector : Editor {
+public class BezierCurveInspector : Editor
+{
 
     private BezierCurve curve;
     private Transform handleTransform;
     private Quaternion handleRotation;
+    private const float handleSize = 0.06f;
+    private const float handlePickSize = 0.1f;
+    private int handleSelectedIndex = -1;
+    private static Color[] modeColors = {
+        Color.white,
+        Color.yellow,
+        Color.cyan
+    };
 
-	private void OnSceneGUI()
+    private void OnSceneGUI()
     {
         curve = target as BezierCurve;
         handleTransform = curve.transform;
         handleRotation = Tools.pivotRotation == PivotRotation.Global ? Quaternion.identity : handleTransform.rotation;
 
         Vector3 p0 = ShowPoint(0);
-        Vector3 p1 = ShowPoint(1);
-        Vector3 p2 = ShowPoint(2);
-        Vector3 p3 = ShowPoint(3);
+        for (int i = 1; i < curve.PointsCount; i += 3) //i+=3 только если добавляем три точки
+        {
+            Vector3 p1 = ShowPoint(i);
+            Vector3 p2 = ShowPoint(i + 1);
+            Vector3 p3 = ShowPoint(i + 2);
 
-        Handles.color = Color.gray;
-        Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p2, p3);
+            Handles.color = Color.gray;
+            Handles.DrawLine(p0, p1);
+            Handles.DrawLine(p2, p3);
 
-        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 4f);
+            Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 4f);
+            p0 = p3;
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        curve = target as BezierCurve;
+        if (handleSelectedIndex >= 0 && handleSelectedIndex < curve.PointsCount)
+        {
+            DrawSelectedPointInspector();
+        }
+        if (GUILayout.Button("Add To Spline"))
+        {
+            Undo.RecordObject(curve, "Add Curve");
+            curve.AddCurve();
+            EditorUtility.SetDirty(curve);
+        }
+    }
+
+    private void DrawSelectedPointInspector()
+    {
+        GUILayout.Label("Selected Point");
+        EditorGUI.BeginChangeCheck();
+        Vector3 point = EditorGUILayout.Vector3Field("Position", curve.GetPoint(handleSelectedIndex));
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(curve, "Move Point");
+            EditorUtility.SetDirty(curve);
+            curve.SetPoint(handleSelectedIndex, point);
+        }
+        EditorGUI.BeginChangeCheck();
+        BezierControlPointMode mode = (BezierControlPointMode)EditorGUILayout.EnumPopup("Mode", curve.GetPointMode(handleSelectedIndex));
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(curve, "Change Point Mode");
+            curve.SetPointMode(handleSelectedIndex, mode);
+            EditorUtility.SetDirty(curve);
+        }
     }
 
     private Vector3 ShowPoint(int index)
     {
-        Vector3 point = handleTransform.TransformPoint(curve.points[index]);
-        EditorGUI.BeginChangeCheck();
-        point = Handles.PositionHandle(point, handleRotation);
-        if(EditorGUI.EndChangeCheck())
+        Vector3 point = handleTransform.TransformPoint(curve.GetPoint(index));
+        float size = HandleUtility.GetHandleSize(point);
+        Handles.color = modeColors[(int)curve.GetPointMode(index)];
+        if (Handles.Button(point, handleRotation, handleSize * size, handlePickSize * size, Handles.CircleCap))
         {
-            Undo.RecordObject(curve, "Move Point");
-            curve.points[index] = handleTransform.InverseTransformPoint(point);
+            handleSelectedIndex = index;
+            EditorUtility.SetDirty(curve);
+        }
+        if (handleSelectedIndex == index)
+        {
+            EditorGUI.BeginChangeCheck();
+            point = Handles.PositionHandle(point, handleRotation);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(curve, "Move Point");
+                curve.SetPoint(index, handleTransform.InverseTransformPoint(point));
+            }
         }
         return point;
     }
